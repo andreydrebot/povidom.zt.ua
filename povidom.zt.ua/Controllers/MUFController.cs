dd9816a8b7f7b4d9ad5b95b37c09160d5ba182b4
Andrey Drebot <a.drebot@gmail.com>
2016-01-24 11:32:42 +0200

    Map with MUFs was added

---
+++ povidom.zt.ua/Controllers/MUFController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace povidom.Controllers
+{
+    public class MUFController : Controller
+    {
+        [Route("mufs")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
